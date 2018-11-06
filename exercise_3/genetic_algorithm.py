ad879d6901ee47b322ea8c51e2a7d28853e6a702
RedFu01 <konradfuger@googlemail.com>
2018-11-06 13:48:12 +0100

    cleanup

--- exercise_3/genetic_algorithm.py
+++ exercise_3/genetic_algorithm.py
@@ -10,52 +10,72 @@ sys.setrecursionlimit(11000)
 def mutate(encoding):
     idx = int(round(random.uniform(0, len(encoding)-1)))
     new_encoding = encoding.copy()
-    new_encoding[idx] = int(round(random.uniform(0, 1)))
-    return new_encoding;
+    new_value = 0
+    if new_encoding[idx] == 0:
+        new_value = 1
+    new_encoding[idx] = new_value
+    return new_encoding
 
 def crossover(e_1, e_2):
     new_encoding = e_2.copy()
     size = int(len(e_1)/2)
-    for i in range( size):
+    for i in range(size):
         new_encoding[i] = e_1[i]
     return new_encoding
 
+def create_encoding_obj(graph, encoding, path_options):
+    return {
+        "encoding": encoding,
+        "cost": get_cost(graph, encoding, path_options)
+    }
+
+def get_random_encoding(length):
+    encoding = np.zeros(length)
+    for i in range(len(encoding)):
+        encoding[i] = int(round(random.uniform(0, 1)))
+    return encoding
+
+def get_new_population(graph, old_population, path_options):
+    new_population = old_population.copy()
+    pop_size = len(old_population)
+    encoding_size = len(old_population[0]["encoding"])
+
+    for _ in range(10):
+        idx_1 = int(round(random.uniform(0, pop_size-1)))
+        idx_2 = int(round(random.uniform(0, pop_size-1)))
+        new_encoding_values = crossover(old_population[idx_1]["encoding"], old_population[idx_2]["encoding"])  
+        new_encoding = create_encoding_obj(graph, new_encoding_values, path_options)
+        new_population.append(new_encoding)
+
+        new_encoding_values = crossover(old_population[idx_2]["encoding"], old_population[idx_1]["encoding"])  
+        new_encoding = create_encoding_obj(graph, new_encoding_values, path_options)
+        new_population.append(new_encoding)
+
+    for _ in range(5):
+        idx = int(round(random.uniform(0, pop_size-1)))
+        new_encoding_values= mutate(old_population[idx]["encoding"])
+        new_encoding = create_encoding_obj(graph, new_encoding_values, path_options)
+        new_population.append(new_encoding)
+
+    for _ in range(5):
+        new_encoding_values= get_random_encoding(encoding_size)
+        new_encoding = create_encoding_obj(graph, new_encoding_values, path_options)
+        new_population.append(new_encoding)
+
+    new_population = sorted(new_population, key=lambda x: x["cost"], reverse=True)
+    size = len(new_population)
+    return new_population[size-11: -1]
+    
+
+
 def do_generation(graph, population, path_options, depth = 0):
     cost = get_cost(graph, population[9]["encoding"], path_options)
-    print(cost)
-    if cost <= 0 or depth > 10000:
+    print('Current best: ', cost)
+    if cost <= 17 or depth > 10000:
         print('DONE')
-        return population[0]["encoding"]
+        return population[9]["encoding"]
     else:
-        new_population = population.copy()
-        for c in range(4):
-            encoding = mutate(population[c]["encoding"])
-            new_encoding = {
-                "encoding": encoding,
-                "cost": get_cost(graph, encoding, path_options)
-            }
-            new_population[c] = new_encoding
-
-        encoding = crossover(population[8]["encoding"], population[9]["encoding"])
-        new_encoding = {
-                "encoding": encoding,
-                "cost": get_cost(graph, encoding, path_options)
-            }
-        new_population[7] = new_encoding
-
-        encoding = crossover(population[9]["encoding"], population[8]["encoding"])
-        new_encoding = {
-                "encoding": encoding,
-                "cost": get_cost(graph, encoding, path_options)
-            }
-        new_population[6] = new_encoding
-
-        new_population = sorted(new_population, key=lambda x: x["cost"], reverse=True)
-        # print('new generation')
-        # for p in new_population:
-        #     print(p["cost"])
-
-        # print('##')
+        new_population = get_new_population(graph, population, path_options)
         do_generation(graph, new_population, path_options, depth +1)
     pass
 
@@ -63,15 +83,15 @@ def do_generation(graph, population, path_options, depth = 0):
 def get_decision_index(arr):
     return int(2 * arr[0] + arr[1])
 
-
 def get_cost(graph, encoding, path_options):
     nodes = graph.nodes()
-    edges = {}
+    edges_in = {}
+    edges_out = {}
 
     idx = 0
     for s in nodes:
         for d in nodes:
-            if d > s:
+            if d != s:
                 current_path_encoding = encoding[idx: idx+2]
                 current_path_decision = get_decision_index(current_path_encoding)
                 key = 'path_' + str(s) + '-' + str(d)
@@ -86,6 +106,10 @@ def get_cost(graph, encoding, path_options):
                 for i in range(len(current_path)-1):
                     link_start = current_path[i]
                     link_end = current_path[i+1]
+                    edges = edges_in
+                    if link_start > link_end:
+                        edges = edges_out
+
                     normalized_indizes = np.sort([link_start, link_end])
                     key = 'edge_' + str(normalized_indizes[0]) + '-' + str(normalized_indizes[1])
                     if key in edges:
@@ -96,12 +120,20 @@ def get_cost(graph, encoding, path_options):
                 idx = idx + 1
     
     cost = 0
-    for key in edges:
-        demand = edges[key]
+    keys = set().union(edges_in.keys(), edges_out.keys())
+    for key in keys:
+        d_1 = 0
+        d_2 = 0
+        if key in edges_in:
+            d_1 = edges_in[key]
+        if key in edges_out:
+            d_2 = edges_out[key]
+            
+        demand = max(d_1, d_2)
+
         c_1 = math.floor(demand / 4)
         c_2 = demand - 4 * c_1
         cost += c_1 * 2.5 + c_2
-    # print(cost)
     return cost
 
 
@@ -114,16 +146,17 @@ def run_genetic_algorithm():
     G.add_edge(2, 5)
     G.add_edge(3, 4)
     G.add_edge(4, 5)
+
     # nx.draw(G, with_labels = True)
     # plt.show()
+
     nodes = G.nodes()
 
     path_options = {}
 
     for s in nodes:
         for d in nodes:
-            if d > s:
-                # print('\n####  ',s,d, '  ####')
+            if d != s:
                 name = 'path_' + str(s) + '-' + str(d)
                 path_options[name] = list(nx.all_simple_paths(G, source=s, target=d))
 
@@ -132,19 +165,13 @@ def run_genetic_algorithm():
     population = []
 
     # initialize with random decision
-    for pop in range(10):
+    for _ in range(10):
         # 2 bit as decision for every encoding
-        encoding = np.zeros(2 * len(path_options))
-        for i in range(len(encoding)):
-            encoding[i] = int(round(random.uniform(0, 1)))
+        encoding_values = get_random_encoding(2 * len(path_options))
+        encoding = create_encoding_obj(G, encoding_values, path_options)
 
-        population.append({
-            "cost": i,#get_cost(G, encoding, path_options),
-            "encoding": encoding
-        })
+        population.append(encoding)
         population = sorted(population, key=lambda x: x["cost"], reverse=True)
-    for p in population:
-        print(p["cost"])
 
     do_generation(G, population, path_options)
     pass
